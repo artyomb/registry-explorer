90aa6b7526dad494334dbeec36d41ef888267cf1
Roman <pugachev.ra@yandex.ru>
2025-01-30 16:06:30 +0300

    Enabled JSON inspection

--- src_registry_explorer/sinatra/sinatra_server.rb
+++ src_registry_explorer/sinatra/sinatra_server.rb
@@ -10,6 +10,7 @@ class RegistryExplorerFront < Sinatra::Base
 
   set :root, '.' #File.dirname(__FILE__)
   get '/',         &->() { slim :index }
+  get '/index',         &->() { slim :index }
   get '/json/:sha256', &->() { slim :json }
   get '/healthcheck', &-> { 'Healthy' }
 
