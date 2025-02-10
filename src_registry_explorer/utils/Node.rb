703a3305f8b6c13b8cfe02d9115f7e62e1990ce0
Roman <pugachev.ra@yandex.ru>
2025-02-10 19:24:19 +0300

    Implemented tag size calculation

--- src_registry_explorer/utils/Node.rb
+++ src_registry_explorer/utils/Node.rb
@@ -1,5 +1,5 @@
 class Node
-  def initialize(type, sha256, node_size = nil)
+  def initialize(type, sha256, node_size = nil, required_blobs = nil, unique_blobs_sizes = nil)
     @type = type
     @sha256 = sha256
     @node_size = node_size
@@ -9,20 +9,21 @@ class Node
     @created_by = nil
     begin
       @actual_blob_size = blob_size(@sha256)
+      unique_blobs_sizes[sha256] = @actual_blob_size unless unique_blobs_sizes.nil?
     rescue Exception => e
       puts "Error: #{e}"
       @actual_blob_size = -1
     end
     return unless @type.to_s =~ /json/
-    find_links JSON.parse blob_content(@sha256), symbolize_names: true
+    find_links(JSON.parse(blob_content(@sha256), symbolize_names: true).merge( {unique_blobs_sizes: unique_blobs_sizes} ))
   end
 
-  def find_links(n, path = [])
+  def find_links(n, path = [], unique_blobs_sizes = nil)
     return unless (n.is_a? Hash or n.is_a? Array)
     if n.is_a? Hash
       n.each do |key, value|
         if value.is_a? Hash
-          add_link(path + [key], value) if value.key? :digest
+          add_link(path + [key], value, unique_blobs_sizes) if value.key? :digest
           find_links(value, path + [key])
         elsif value.is_a? Array
           find_links(value, path + [key])
@@ -31,17 +32,17 @@ class Node
     elsif n.is_a? Array
       n.each_with_index do |sub_hash, id|
         if sub_hash.is_a? Hash
-          add_link(path + ["[#{id}]"], sub_hash) if sub_hash.key? :digest
+          add_link(path + ["[#{id}]"], sub_hash, unique_blobs_sizes) if sub_hash.key? :digest
           find_links(sub_hash, path + ["[#{id}]"])
         end
       end
     end
   end
 
-  def add_link(path, value)
+  def add_link(path, value, unique_blobs_sizes = nil)
     digest_val = value[:digest]
     sha256 = digest_val.is_a?(Hash) ? digest_val[:sha256] : digest_val.split(':').last
-    @links << { path:, node: Node.new(value[:mediaType], sha256, value[:size]) }
+    @links << { path:, node: Node.new(value[:mediaType], sha256, value[:size], nil, unique_blobs_sizes) }
     puts("Noode #{sha256} with type #{@type} added link to node #{@links.last}")
   end
 
@@ -71,5 +72,22 @@ class Node
 
   def created_by
     @created_by
+  end
+
+  def get_included_blobs(included_blobs = Set.new)
+    included_blobs.add(@sha256)
+    @links.each do |link|
+      link[:node].get_included_blobs(included_blobs)
+    end
+    included_blobs
+  end
+
+  def get_size_deep(unique_blobs_sizes = nil)
+    included_blobs = get_included_blobs
+    size_deep = 0
+    included_blobs.each do |blob_sha256|
+      size_deep += unique_blobs_sizes[blob_sha256] unless unique_blobs_sizes.nil?
+    end
+    size_deep
   end
 end
--- src_registry_explorer/utils/file_utils.rb
+++ src_registry_explorer/utils/file_utils.rb
@@ -8,7 +8,12 @@ def blob_content(sha256)
 end
 
 def blob_size(sha256)
-  File.size $base_path + "/blobs/sha256/#{sha256[0..1]}/#{sha256}/data"
+  begin
+    File.size $base_path + "/blobs/sha256/#{sha256[0..1]}/#{sha256}/data"
+  rescue Exception => e
+    puts "Error: #{e}"
+    -1
+  end
 end
 
 def extract_tar_gz_structure(tar_gz_sha256)
@@ -50,23 +55,24 @@ def extract_tar_gz_structure(tar_gz_sha256)
 end
 
 
-def extract_tag_with_image(tag_path, base_path, image_name, current_img)
+def extract_tag_with_image(tag_path, base_path, image_name, current_img, unique_blobs_sizes=nil)
   puts("Image #{image_name}")
   puts("Founded tag #{tag_path}")
-  current_tag = { name: tag_path.split('/').last, index_Nodes: [], current_index_sha256: File.read(tag_path + "/current/link").split(':').last }
+  current_tag = { name: tag_path.split('/').last, index_Nodes: [], current_index_sha256: File.read(tag_path + "/current/link").split(':').last, required_blobs: Set.new, size: -1, problem_blobs: [] }
   current_img[:tags] << current_tag
   indexes_paths = Dir.glob(tag_path + "/index/sha256/*")
-  extract_index(current_tag[:current_index_sha256], base_path, current_tag)
+  extract_index(current_tag[:current_index_sha256], base_path, current_tag, unique_blobs_sizes)
   indexes_paths.each do |index_path|
     index_sha256 = index_path.split('/').last
     next if index_sha256 == current_tag[:current_index_sha256]
-    extract_index(index_sha256, base_path, current_tag)
+    extract_index(index_sha256, base_path, current_tag, unique_blobs_sizes)
   end
-  puts
+  calculate_tag_size(current_tag)
+  current_tag
 end
 
-def extract_tag(tag_path, base_path)
-  current_tag = { name: tag_path.split('/').last, index_Nodes: [], current_index_sha256: nil, size: nil, created_at: nil }
+def extract_tag_without_image(tag_path, base_path)
+  current_tag = { name: tag_path.split('/').last, index_Nodes: [], current_index_sha256: nil, created_at: nil, required_blobs: Set.new, size: -1, problem_blobs: [] }
   begin
     current_tag[:current_index_sha256] = File.read(tag_path + "/current/link").split(':').last
   rescue Exception => e
@@ -80,16 +86,16 @@ def extract_tag(tag_path, base_path)
     next if index_sha256 == current_tag[:current_index_sha256]
     extract_index(index_sha256, base_path, current_tag)
   end
-  puts
+  calculate_tag_size(current_tag)
   current_tag
 end
 
-def extract_index(index_sha256, base_path, current_tag)
+def extract_index(index_sha256, base_path, current_tag, unique_blobs_sizes = nil)
   outer_index_path = base_path + "/blobs/sha256/#{index_sha256[0..1]}/#{index_sha256}/data"
   index_content = JSON.parse(File.read(outer_index_path))
-  puts("Tag has index #{index_sha256} with content in blob: #{JSON.pretty_generate(index_content)}")
-  current_Node_link = { path: ["Image"], node: Node.new(index_content["mediaType"], index_sha256, index_content[:size]), parent_sha256: index_sha256 }
+  current_Node_link = { path: ["Image"], node: Node.new(index_content["mediaType"], index_sha256, index_content[:size], nil, unique_blobs_sizes), parent_sha256: index_sha256 }
   current_tag[:index_Nodes] << current_Node_link
+  current_tag[:required_blobs].merge(current_Node_link[:node].get_included_blobs)
 end
 
 def define_create_time(sha256)
@@ -120,4 +126,22 @@ end
 
 def represent_size(size_in_bytes)
   size_in_bytes.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\\1,')
+end
+
+def calculate_tag_size(current_tag)
+  size_of_tag = 0
+  current_tag[:required_blobs].each do |blob|
+    begin
+      current_blob_size = blob_size(blob)
+      if current_blob_size == -1
+        current_tag[:problem_blobs] << blob
+        raise Exception.new("Blob #{blob} not founded")
+      end
+      size_of_tag += current_blob_size
+    rescue Exception => e
+      puts("Error: #{e.message}")
+    end
+  end
+  current_tag[:size] = size_of_tag
+  size_of_tag
 end
