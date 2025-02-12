25ad6c61a2accf326db9c325ce986ffa67614b66
Roman <pugachev.ra@yandex.ru>
2025-02-12 18:11:39 +0300

    Added href to tags

--- src_registry_explorer/utils/file_utils.rb
+++ src_registry_explorer/utils/file_utils.rb
@@ -200,9 +200,16 @@ def extract_index_created_at(sha256)
   date
 end
 
-def get_referring_images(blob_sha256)
+def get_referring_image_entries(blob_sha256)
   images = []
   extract_images(images)
-  result = images.select { |image| image[:required_blobs].include?(blob_sha256) }
-  result
+  result_list = []
+  images.select { |image| image[:required_blobs].include?(blob_sha256) }.each do |image|
+    image[:tags].select{ |tag| tag[:required_blobs].include?(blob_sha256)}.each do |tag|
+      tag[:index_Nodes].map{|node_link| node_link[:node]}.select{|cn| cn.get_included_blobs.include?(blob_sha256)}.each do |index_node|
+        result_list << {image_name: image[:name], tag_name: tag[:name], index_node: index_node}
+      end
+    end
+  end
+  result_list
 end
