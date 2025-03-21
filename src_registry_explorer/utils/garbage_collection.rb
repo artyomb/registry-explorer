b2f5feed940733cd4e8787c598fd5656c2ac9b0d
Roman <pugachev.ra@yandex.ru>
2025-03-21 20:31:17 +0300

    Adjustments of gc with history deleting

--- src_registry_explorer/utils/garbage_collection.rb
+++ src_registry_explorer/utils/garbage_collection.rb
@@ -6,23 +6,19 @@ require_relative 'file_utils'
 
 def get_images_strictire_where_indexes_without_revisions(images_structure = Set.new)
   images_paths = get_images_paths
-  TimeMeasurer.measure(:images_paths_after) do
-    images_paths.each do |image_path|
-      subfolders = image_path.split('/')
-      image_name = "/" + subfolders[subfolders.find_index('repositories') + 1..].join('/')
-      current_img = { name: image_name, tags: Set.new, total_size: -1, required_blobs: Set.new, problem_blobs: Set.new }
-      tag_paths = Dir.glob(image_path + "/_manifests/tags/*").select { |f| File.directory?(f) }
-      TimeMeasurer.measure(:creating_tags) do
-        tag_paths.map { |tag_path| extract_tag_with_unlinked_indexes(tag_path) }.each do |tag|
-          current_img[:tags].add(tag)
-          current_img[:required_blobs].merge(tag[:required_blobs])
-          current_img[:problem_blobs].merge(tag[:problem_blobs])
-        end
-      end
-      next if current_img[:tags].empty?
-      images_structure.add current_img
-      current_img[:total_size] = CachesManager.get_repo_size(image_path, current_img[:required_blobs])
+  images_paths.each do |image_path|
+    subfolders = image_path.split('/')
+    image_name = "/" + subfolders[subfolders.find_index('repositories') + 1..].join('/')
+    current_img = { name: image_name, tags: Set.new, total_size: -1, required_blobs: Set.new, problem_blobs: Set.new }
+    tag_paths = Dir.glob(image_path + "/_manifests/tags/*").select { |f| File.directory?(f) }
+    tag_paths.map { |tag_path| extract_tag_with_unlinked_indexes(tag_path) }.each do |tag|
+      current_img[:tags].add(tag)
+      current_img[:required_blobs].merge(tag[:required_blobs])
+      current_img[:problem_blobs].merge(tag[:problem_blobs])
     end
+    next if current_img[:tags].empty?
+    images_structure.add current_img
+    current_img[:total_size] = CachesManager.get_repo_size(image_path, current_img[:required_blobs])
   end
   images_structure
 end
@@ -45,11 +41,9 @@ def extract_tag_with_unlinked_indexes(tag_path)
   index_without_problems.sort_by! { _1[:node]&.created_at || '-' }.reverse!
   current_tag[:index_Nodes].append(*index_without_problems)
   current_tag[:index_Nodes].append(*problem_indexes)
-  TimeMeasurer.measure(:search_tags_blobs) do
-    current_tag[:index_Nodes].map do |index_node|
-      current_tag[:required_blobs].merge index_node[:node].get_included_blobs
-      current_tag[:problem_blobs].merge index_node[:node].get_problem_blobs
-    end
+  current_tag[:index_Nodes].map do |index_node|
+    current_tag[:required_blobs].merge index_node[:node].get_included_blobs
+    current_tag[:problem_blobs].merge index_node[:node].get_problem_blobs
   end
   current_tag[:size] = CachesManager.get_repo_size(tag_path, current_tag[:required_blobs])
   current_tag
@@ -78,4 +72,35 @@ def get_images_structure_affected_by_gc(images_structure = Set.new, required_blo
   end
 
   images_structure
+end
+
+def get_images_structure_tags_wth_only_current(images_structure = Set.new)
+  images_paths = get_images_paths
+  images_paths.each do |image_path|
+    subfolders = image_path.split('/')
+    image_name = "/" + subfolders[subfolders.find_index('repositories') + 1..].join('/')
+    current_img = { name: image_name, tags: Set.new, total_size: -1, required_blobs: Set.new, problem_blobs: Set.new }
+    tag_paths = Dir.glob(image_path + "/_manifests/tags/*").select { |f| File.directory?(f) }
+    tag_paths.map { |tag_path| extract_tag_current_only(tag_path) }.each do |tag|
+      current_img[:tags].add(tag)
+      current_img[:required_blobs].merge(tag[:required_blobs])
+      current_img[:problem_blobs].merge(tag[:problem_blobs])
+    end
+    next if current_img[:tags].empty?
+    images_structure.add current_img
+    current_img[:total_size] = CachesManager.get_repo_size(image_path, current_img[:required_blobs])
+  end
+  images_structure
+end
+
+def extract_tag_current_only(tag_path)
+  current_tag = { name: tag_path.split('/').last, index_Nodes: [], current_index_sha256: CachesManager.get_index_sha256(tag_path + "/current/link"), required_blobs: Set.new, size: -1, problem_blobs: Set.new }
+  indexes_paths = Dir.glob(tag_path + "/index/sha256/*")
+  current_tag[:index_Nodes] << extract_index(current_tag[:current_index_sha256])
+  current_tag[:index_Nodes].map do |index_node|
+    current_tag[:required_blobs].merge index_node[:node].get_included_blobs
+    current_tag[:problem_blobs].merge index_node[:node].get_problem_blobs
+  end
+  current_tag[:size] = CachesManager.get_repo_size(tag_path, current_tag[:required_blobs])
+  current_tag
 end
