f871bce56825bdb9c01cb80919d3d9db3bcd55b2
Roman <pugachev.ra@yandex.ru>
2025-04-29 16:37:14 +0300

    Fixed call 'dig' for NULL tag information

---
+++ src_registry_explorer/utils/index_page_help_utils.rb
@@ -0,0 +1,29 @@
+require 'benchmark'
+require_relative '../utils/Node'
+require_relative '../utils/file_utils'
+require_relative '../utils/time_measurer'
+
+def load_tree
+  images = Set.new
+  TimeMeasurer.start_measurement
+
+  tree = { children: {}, image: {}, total_images_amount: 0, required_blobs: Set.new, problem_blobs: Set.new }
+
+  flattened = nil
+  extract_images(images)
+  required_in_registry_blobs = Set.new
+  problem_blobs = Set.new
+  images.each do |image|
+    required_in_registry_blobs.merge(image[:required_blobs])
+    problem_blobs.merge(image[:problem_blobs])
+  end
+  TimeMeasurer.measure(:building_tree) do
+    build_tree(images, tree)
+  end
+  TimeMeasurer.measure(:flatten_tree) do
+    flattened = flatten_tree(tree)
+  end
+  puts TimeMeasurer.log_measurers
+  puts "Flattened tree size: #{flattened.size}"
+  [flattened, images, problem_blobs, required_in_registry_blobs]
+end
